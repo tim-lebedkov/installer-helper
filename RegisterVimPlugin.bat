7534faee61b6eb0366b2bf393bc65886ae69374e
Tim Lebedkov <tim.lebedkov@gmail.com>
2014-09-13 10:51:49 +0200

    use also the version number for registering Vim plugins

--- RegisterVimPlugin.bat
+++ RegisterVimPlugin.bat
@@ -1,9 +1,10 @@
 rem This script is available since version 1.4
 rem This script registers the current packages as a Vim plugin.
+rem Since 1.5 this script uses the package version in the name of the link.
 rem 
 rem Script parameters: none
 rem
 mkdir "%ALLUSERSPROFILE%/Npackd/VimPlugins/"
 
-mklink /D "%ALLUSERSPROFILE%/Npackd/VimPlugins/%NPACKD_PACKAGE_NAME%" "%CD%"
+mklink /D "%ALLUSERSPROFILE%/Npackd/VimPlugins/%NPACKD_PACKAGE_NAME%-%NPACKD_PACKAGE_VERSION%" "%CD%"
 

8c96cdec37fa6f36cc4bc7a67e4957aa3aee7bf9
Tim Lebedkov <tim.lebedkov@gmail.com>
2014-09-13 10:51:49 +0200

    use also the version number for registering Vim plugins

--- UnregisterVimPlugin.bat
+++ UnregisterVimPlugin.bat
@@ -1,6 +1,7 @@
 rem This script is available since version 1.4
 rem This script unregisters the current package as a Vim plugin.
+rem Since 1.5 this script uses the package version in the name of the link.
 rem
-rmdir "%ALLUSERSPROFILE%/Npackd/VimPlugins/%NPACKD_PACKAGE_NAME%"
+rmdir "%ALLUSERSPROFILE%/Npackd/VimPlugins/%NPACKD_PACKAGE_NAME%-%NPACKD_PACKAGE_VERSION%"
 verify
 

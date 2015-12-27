2b996d18b81296cf1eff4af27a51eb68a2cd9f60
Tim Lebedkov <tim.lebedkov@gmail.com>
2015-12-27 19:29:47 +0100

    Lib.js

---
+++ Lib.js
@@ -0,0 +1,37 @@
+/**
+ * This script is available since version 1.10.
+ *
+ * It contains useful code for installing and removing software and can be 
+ * used via cscript.exe e.g. from .Npackd\Install.bat:
+ *     cscript //Nologo //B //E:JScript //T:300 //U .Npackd\Install.js
+ *
+ * ...and in the Install.js script (the environment variable "NIH" should point
+ * to a valid directory with Installer Helper):
+ *     var fs = new ActiveXObject("Scripting.FileSystemObject");
+ *     var sh = WScript.CreateObject("WScript.Shell");
+ *     var lib = eval(fs.OpenTextFile(
+ *          sh.ExpandEnvironmentStrings("%NIH%") + "\\Lib.js", 1).ReadAll());
+ *
+ *     lib.sendKeysToWindow("NetSurf - NetSurf Uninstall", "{ENTER}");
+ */
+(function () {
+
+var L = {/* library interface */};
+
+/**
+ * Wait for the dialog with the specified title and presses ENTER.
+ *
+ * @param title dialog title
+ * @param keys e.g. "{ENTER}". See WScript.SendKeys for more details
+ */
+L.sendKeysToWindow = function(title, keys) {
+    var sh = WScript.CreateObject("WScript.Shell");
+    WScript.Sleep(60000);
+    sh.AppActivate(title);
+    WScript.Sleep(10000);
+    sh.SendKeys(keys);
+};
+return L;
+
+}).call()
+

496def17fe34db0650ac6f2cc7904eeedd69ad9d
Tim Lebedkov <tim.lebedkov@gmail.com>
2016-08-21 16:11:06 +0200

    comment

--- Extract.bat
+++ Extract.bat
@@ -1,5 +1,6 @@
 rem This script is available since version 1.19
 rem This script extracts .7z, .tar, .gz, .tar.gz or .tar.xz file or a directory.
+rem The original file or directory will be deleted.
 rem 
 rem Script parameters:
 rem %1 - name of the file or directory. The file extension will be used to 

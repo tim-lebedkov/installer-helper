5d1b8719fc7497ea489ebee5c074895dba7a19a7
tim-lebedkov <tim.lebedkov@gmail.com>
2019-09-09 21:45:47 +0200

    deprecate DownloadOracle.bat

--- DownloadOracle.bat
+++ DownloadOracle.bat
@@ -1,3 +1,6 @@
+rem WARNING: since version 1.23 this script is deprecated and should not be used.
+rem Please use the OpenJDK package instead.
+rem 
 rem since 1.22
 rem This script downloads a file from http://download.oracle.com automatically
 rem accepting the Oracle license.
